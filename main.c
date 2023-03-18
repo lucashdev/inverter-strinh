ed948a3e259a985f15ab40665ea639c5eaf8d99f
Lucas Henrique <124297186+lucashdev@users.noreply.github.com>
2023-03-18 17:20:47 -0300

    string

---
+++ main.c
@@ -0,0 +1,39 @@
+void inverter(char vet[]){
+    int i, aux, fim, tam = strlen(vet);
+
+    fim = tam - 1;
+    for(i = 0; i < tam/2; i++){
+        aux = vet[i];
+        vet[i] = vet[fim];
+        vet[fim] = aux;
+        fim--;
+    }
+}
+
+char* inverter2(char vet[]){
+    int i, aux, fim, tam = strlen(vet);
+    char *novo = malloc((tam + 1) * sizeof(char));
+
+    fim = tam - 1;
+    for(i = 0; i < tam; i++){
+        novo[fim] = vet[i];
+        fim--;
+    }
+    novo[i] = '\0';
+    return novo;
+}
+
+int main(){
+
+    char palavra[25] = {"Quero a vaga"};
+    char *p;
+
+    printf("%s\n", palavra);
+    //inverter(palavra);
+    printf("%s\n", palavra);
+
+    p = inverter2(palavra);
+    printf("%s\n", p);
+
+    return 0;
+}
